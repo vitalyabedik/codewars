cfb26ed90ba95842e54a3886fd46adafb0c7df88
Vitali Bedik <vitalya.bedik@gmail.com>
2022-12-31 16:30:00 +0300

    RS School - stage #0 - fakeBinary

---
+++ 8kyu/fakeBinary.js
@@ -0,0 +1,15 @@
+// kata
+
+/* Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. 
+Return the resulting string.
+
+Note: input will never be an empty string */
+
+// solution
+
+function fakeBin(x) {
+  return x
+    .split('')
+    .map(el => (el < 5 ? 0 : 1))
+    .join('');
+}
