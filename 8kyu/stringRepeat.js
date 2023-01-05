90103a18e83bfe91a4afa4086fefb5602dddd094
Vitali Bedik <vitalya.bedik@gmail.com>
2023-01-05 19:32:20 +0300

    RS School - stage #0 - stringRepeat

---
+++ 8kyu/stringRepeat.js
@@ -0,0 +1,10 @@
+// kata
+
+/* 
+Write a function that accepts an integer n and a string s as parameters, 
+and returns a string of s repeated exactly n times. 
+*/
+
+// solution
+
+const repeatStr = (n, s) => s.repeat(n);
