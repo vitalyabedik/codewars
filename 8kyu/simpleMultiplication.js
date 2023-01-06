49cda17c2661fc9f5f7da608c919d42c64d8059f
Vitali Bedik <vitalya.bedik@gmail.com>
2023-01-06 17:14:55 +0300

    RS School - stage #0 - simpleMultiplication

---
+++ 8kyu/simpleMultiplication.js
@@ -0,0 +1,10 @@
+// kata
+
+/* 
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+*/
+
+// solution
+
+const simpleMultiplication = number =>
+  number % 2 === 0 ? number * 8 : number * 9;
